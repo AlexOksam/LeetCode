7de75ce1c6549e8cdc60244eb18ed457b90c66ea
Alexander Masko <117868235+AlexOksam@users.noreply.github.com>
2025-01-19 21:21:46 +0100

    Create 709_To_Lower_Case.cs

---
+++ Programming_Skills/709_To_Lower_Case.cs
@@ -0,0 +1,33 @@
+// SOLUTION
+public class Solution {
+    public string ToLowerCase(string s) {
+
+        return s.ToLower();
+        
+    }
+}
+
+/* PROBLEM
+Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+ 
+
+Example 1:
+
+Input: s = "Hello"
+Output: "hello"
+Example 2:
+
+Input: s = "here"
+Output: "here"
+Example 3:
+
+Input: s = "LOVELY"
+Output: "lovely"
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s consists of printable ASCII characters.
+*/
